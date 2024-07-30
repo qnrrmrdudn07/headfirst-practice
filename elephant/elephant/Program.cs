a9f2ac77688bed5269f82fb80c004c4a363fd7ba
qnrrmrdudn07 <chltjswo1357@gmail.com>
2024-07-30 14:22:06 +0900

    7/30

---
+++ elephant/elephant/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+internal class Elephant
+{
+    public int EarSize;
+    public string? Name;
+    public void WhoAmI()
+    {
+        Console.WriteLine("My name is " + Name + ".");
+        Console.WriteLine("My earsize is " + EarSize + ".");
+
+    }
+    static void Main(string[] args)
+    {
+        Elephant luc = new Elephant() { Name = "luc", EarSize = 33 };
+        Elephant llo = new Elephant() { Name = "llo", EarSize = 44 };
+        while (true)
+        {
+            char input = Console.ReadKey(true).KeyChar;
+            if (input == '1')
+            {
+                Console.WriteLine("calling who am i(luc)");
+                luc.WhoAmI();
+            }
+            else if (input == '2')
+            {
+                Console.WriteLine("calling who am i (llo)");
+                llo.WhoAmI();
+            }
+            else if (input == '3')
+            {
+                Console.WriteLine("swapping");
+                Elephant tmp;
+                tmp = llo;
+                llo = luc;
+                luc = tmp;
+
+            }
+            else
+            {
+                return;
+            }
+            Console.WriteLine();
+        }
+    }
+}
+
