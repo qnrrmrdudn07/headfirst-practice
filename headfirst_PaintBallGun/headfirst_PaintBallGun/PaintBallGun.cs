9b0f37e363b4c18d48f9559d7da4a1aab3446df5
qnrrmrdudn07 <chltjswo1357@gmail.com>
2024-08-09 16:03:01 +0900

    8/9
    2

---
+++ headfirst_PaintBallGun/headfirst_PaintBallGun/PaintBallGun.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace headfirst_PaintBallGun
+{
+    internal class PaintBallGun
+    {
+        public const int MAGAZINE_SIZE = 16;
+
+        private int balls = 0; 
+        private int ballsLoaded = 0;
+
+        public int GetBallsLoaded() { return ballsLoaded; }
+        public bool IsEmpty() { return ballsLoaded == 0; }
+        public int GetBalls() { return balls; }
+        
+        public void SetBalls(int numberOfBalls)
+        {
+            if (numberOfBalls > 0)
+                ballsLoaded = numberOfBalls;
+            Reload();
+        }
+        public void Reload()
+        {
+            if (balls > MAGAZINE_SIZE)
+                ballsLoaded = MAGAZINE_SIZE;
+            else
+                ballsLoaded = balls;
+        }
+
+        public bool Shoot()
+        {
+            if (ballsLoaded == 0) return false;
+            ballsLoaded--;
+            balls--;
+            return true;
+        }
+    }
+}
---
+++ headfirst_PaintBallGun/headfirst_PaintBallGun/Program.cs
@@ -0,0 +1,23 @@
+﻿using headfirst_PaintBallGun;
+using System.ComponentModel.Design;
+
+class Program
+{
+    static void Main(string[] args) // 여기엔 왜 public 안붙음?
+    {
+        PaintBallGun gun = new PaintBallGun();
+        while (true)
+        {
+            Console.WriteLine($"{gun.GetBalls()} balls, {gun.GetBallsLoaded()} loaded");
+            if (gun.IsEmpty()) Console.WriteLine("WARNING");
+            Console.WriteLine("space to shoot, r to reload, + to add ammo, q to quit");
+            char key = Console.ReadKey(true).KeyChar;
+            
+            if (key == ' ') Console.WriteLine($"returned {gun.Shoot()}");
+            else if (key == 'r') gun.Reload();
+            else if (key == '+') gun.SetBalls(gun.GetBalls() + PaintBallGun.MAGAZINE_SIZE);
+            else if (key == 'q') return;
+        }
+
+    }
+}
